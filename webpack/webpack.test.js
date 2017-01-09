6d7d22cdd17f14eb6f68b09385406c9faecab711
Giovanni Silva <giovanni@atende.info>
2017-01-09 20:59:27 -0200

    test infraestructure remaded

---
+++ webpack/webpack.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+var tscompilerOptions = {
+    compilerOptions: {
+        "module": "commonjs"
+    }
+};
+
+module.exports = {
+    devtool: 'inline-source-map',
+    module: {
+        preLoaders: [
+            { exclude: /node_modules/, loader: 'tslint', test: /\.tsx?$/ }
+        ],
+        loaders: [
+            { exclude: '/node_modules', loader: 'raw', test: /\.(css|html)$/ },
+            { exclude: '/node_modules', loader: 'ts-loader?' + JSON.stringify(tscompilerOptions), test: /\.tsx?$/ }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts', '.tsx'],
+        modulesDirectories: ['node_modules'],
+        root: path.resolve('.', 'src')
+    },
+    tslint: {
+        emitErrors: true
+    }
+};
