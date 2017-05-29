a399950a501ca57ee0b4e4c46959edb8273b6790
Giovanni Silva <giovanni@atende.info>
2017-05-28 21:04:01 -0300

    fix #13

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -6,5 +6,5 @@ import { Observable } from "rxjs/Observable"
 export abstract class Interceptor {
     abstract before(request: Request): Observable<any>;
     abstract after(response: Response): void;
-    abstract error(err: any): void;
+    abstract error(err: Response): void;
 }
